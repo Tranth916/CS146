26ae0696b287442d87d89f30f1abaae80c9c3ed4
thao <tranth916@gmail.com>
2016-09-27 13:58:00 -0700

    Insertionsort works for data.txt string[]

--- Sort.java
+++ Sort.java
@@ -1,32 +1,90 @@
 import java.util.Random;
 import java.lang.*;
-
+import java.io.*;
+import java.util.*;
 public class Sort
 {
-public static void main(String[]args)
+	public static void main(String[]args)
 	{	
-		Random numberGenerator = new Random();
+		String[] names = insertsort();
+		String[] sortedNames = new String[names.length];
 
-		int[] input = new int[10000];
+		sortedNames = insertsort();
 
-		for(int i = 0; i < 10000; i++)
+		insertionSort(sortedNames);
+
+		for(int i = 0 ; i < names.length; i++)
 		{
-			int n = numberGenerator.nextInt(10000);
-			input[i] = n;
-			//System.out.print(input[i] + " ");
+			System.out.print(names[i] +" \t\t\t\t" + sortedNames[i]);
+			System.out.println();
 		}
-
-		long startTime = System.currentTimeMillis();
-		//insertionSort(input);
-		//mergesort(input);
-		long endTime = System.currentTimeMillis();
-
-		System.out.println();
-
-		System.out.println("Started: " + startTime + "Ended: " + endTime +
-							"Total Time to Complete: " + (endTime - startTime) );
-
 	}
+
+public static int getNumber(String str)
+{
+	StringBuilder num = new StringBuilder();
+	for(int i = 0; i < str.length(); i++)
+	{
+		if(Character.isDigit(str.charAt(i)))
+		{
+			num.append(str.charAt(i));
+		}
+		if(str.charAt(i) == ' ')
+		{
+			i = str.length();
+		}
+	}
+	return Integer.parseInt(num.toString());
+}
+
+
+public static String[] insertsort()
+{
+	ArrayList<String> data = new ArrayList<String>();
+	String fileName = "data.txt";
+	String line = null;
+	try{
+		FileReader filereader = new FileReader(fileName);
+		BufferedReader bufferedreader = new BufferedReader(filereader);
+		while( (line = bufferedreader.readLine()) != null)
+		{
+			data.add(line);
+		}
+		bufferedreader.close();
+	}
+	catch(FileNotFoundException e)
+	{	
+		System.out.println("File not found!");
+	}
+	catch(IOException ex)
+	{
+		System.out.println("End of File");
+	}
+	String[] names = new String[data.size()];
+	names = data.toArray(names);
+	return names;
+}
+
+public static void insertionSort(String[] A)
+{
+	for(int i = 1; i < A.length; i++)
+	{
+		int j = i;
+
+		int currentKeyIndex = getNumber(A[j]);
+		String currentKeyString = A[j];
+
+		for(j = i - 1; (j >= 0 && getNumber(A[j]) > currentKeyIndex); j-- )
+		{
+			if(getNumber(A[j]) > currentKeyIndex)
+			{
+				A[j+1] = A[j];
+			}
+		}
+		A[j+1] = currentKeyString;
+	}
+
+}
 
 public static void insertionSort(int[] A)
 	{
@@ -40,7 +98,7 @@ public static void insertionSort(int[] A)
 			int currentKey = A[j];
 			
 			//traverse array in right to left using j.
-			for(j = i - 1; (j >= 0 && A[j] > currentKey) ; j--)
+			for(j = i - 1; j >= 0 && A[j] > currentKey; j--)
 			{
 				//Finding a place to insert values greater than
 				//Key that is currently on the left of key.
@@ -56,8 +114,6 @@ public static void insertionSort(int[] A)
 		}
 
 	}
-
-
 
 //The helper method.
 public static void mergesort(int[] A)
