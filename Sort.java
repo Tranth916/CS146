42fd20cbc46d91926b5b0674b8f118ebdaa3bf3b
thao <tranth916@gmail.com>
2016-09-27 18:22:40 -0700

    sad

--- Sort.java
+++ Sort.java
@@ -2,181 +2,126 @@ import java.util.Random;
 import java.lang.*;
 import java.io.*;
 import java.util.*;
+
 public class Sort
 {
 	public static void main(String[]args)
 	{	
-		String[] names = insertsort();
-		String[] sortedNames = new String[names.length];
+		
 
-		sortedNames = insertsort();
-
-		insertionSort(sortedNames);
-
-		for(int i = 0 ; i < names.length; i++)
-		{
-			System.out.print(names[i] +" \t\t\t\t" + sortedNames[i]);
-			System.out.println();
-		}
 	}
 
-public static int getNumber(String str)
+public static String[] getCommand()
 {
-	StringBuilder num = new StringBuilder();
-	for(int i = 0; i < str.length(); i++)
+	System.out.print("Type in insertionsort data.txt\n");		
+
+	Scanner inputCommand = new Scanner(System.in);
+		
+	String givenCommand = inputCommand.nextLine();
+
+	if(givenCommand != null)
 	{
-		if(Character.isDigit(str.charAt(i)))
+		String[] splitCmd = givenCommand.split("\\s+");
+		if(splitCmd[0].matches("insertionsort") || splitCmd[0].matches("mergesort"))
 		{
-			num.append(str.charAt(i));
-		}
-		if(str.charAt(i) == ' ')
-		{
-			i = str.length();
+			return splitCmd; 
 		}
 	}
-	return Integer.parseInt(num.toString());
-}
-
-
-public static String[] insertsort()
+	return null;public class mergesort<T>
 {
-	ArrayList<String> data = new ArrayList<String>();
-	String fileName = "data.txt";
-	String line = null;
-	try{
-		FileReader filereader = new FileReader(fileName);
-		BufferedReader bufferedreader = new BufferedReader(filereader);
-		while( (line = bufferedreader.readLine()) != null)
-		{
-			data.add(line);
-		}
-		bufferedreader.close();
-	}
-	catch(FileNotFoundException e)
-	{	
-		System.out.println("File not found!");
-	}
-	catch(IOException ex)
+void mergesort(T[] A)
 	{
-		System.out.println("End of File");
-	}
-	String[] names = new String[data.size()];
-	names = data.toArray(names);
-	return names;
-}
-
-public static void insertionSort(String[] A)
-{
-	for(int i = 1; i < A.length; i++)
-	{
-		int j = i;
-
-		int currentKeyIndex = getNumber(A[j]);
-		String currentKeyString = A[j];
-
-		for(j = i - 1; (j >= 0 && getNumber(A[j]) > currentKeyIndex); j-- )
-		{
-			if(getNumber(A[j]) > currentKeyIndex)
-			{
-				A[j+1] = A[j];
-			}
-		}
-		A[j+1] = currentKeyString;
-	}
-
-}
-
-public static void insertionSort(int[] A)
-	{
-		//Assume index 0 is sorted.
-		//Traverse the entire Array. From left to right.
-		for(int i = 1; i < A.length; i++)
-		{
-			//Initialize j as the place to start.
-			int j = i;
-			//Initialize a Key which remembers the value in A[j].
-			int currentKey = A[j];
-			
-			//traverse array in right to left using j.
-			for(j = i - 1; j >= 0 && A[j] > currentKey; j--)
-			{
-				//Finding a place to insert values greater than
-				//Key that is currently on the left of key.
-				if(A[j] > currentKey)
-				{
-					//Shift all values to the right until we find a place
-					//To insert the current key.
-					A[j+1] = A[j];
-				}
-			}
-			//Place is found to insert key. 
-			A[j+1] = currentKey;
-		}
-
-	}
-
-//The helper method.
-public static void mergesort(int[] A)
-{
 	//Basecase: Array length is 1.
 	if(A.length > 1)
 	{
 		int endOfHead = A.length / 2;
-		
 		int lengthOfTail = A.length - (A.length/2);
-		
-		int[] head = new int[endOfHead];
-
+		T[] head = (T[]) new Object[endOfHead];
 		System.arraycopy(A, 0, head, 0, endOfHead);
-
-		int[] tail = new int[lengthOfTail];
-
+		T[] tail = (T[]) new Object[lengthOfTail];
 		System.arraycopy(A, endOfHead, tail, 0, lengthOfTail);
-
 		mergesort(head);
 		mergesort(tail);
 		merge(head,tail,A);
 	}
-
 }
 
-public static void merge(int[] A, int[] B, int[] C)
+public int compareTo(T str1 ,T str2)
 {
+	String first = String.valueOf(str1);
+	String second = String.valueOf(str2);
 
+
+	String[] spl1 = first.trim().split("\\s+");	
+	String[] spl2 = second.trim().split("\\s+");
+	try
+	{	
+		int strOne = Integer.parseInt(spl1[0]);
+		int strTwo = Integer.parseInt(spl2[0]);
+
+		if(strOne > strTwo)
+		{
+			return 1;
+		}
+		else if(strOne < strTwo)
+		{
+			return -1;
+		}
+		else if(strOne == strTwo)
+		{
+			return 0;
+		}
+
+	}
+	catch(NumberFormatException e)
+	{
+
+	}
+	return 0;
+}
+
+
+public void merge(T[] Head, T[] Tail, T[] Original)
+{
 	int aPointer = 0;
 	int bPointer = 0;
 	int cPointer = 0;
-
-	while(aPointer < A.length && bPointer < B.length)
+	while(aPointer < Head.length && bPointer < Tail.length)
 		{
-			if(A[aPointer] < B[bPointer])
+			//Head < Tail
+			if( compareTo( Head[aPointer] , Tail[bPointer]) < 0)
 			{
-				C[cPointer] = A[aPointer];
+				Original[cPointer] = Head[aPointer];
 				cPointer++;
 				aPointer++;
 			}
-			else if (B[bPointer] < A[aPointer])
+			//Head > Tail
+			else if ( compareTo( Head[aPointer], Tail[bPointer]) > 0 )
 			{
-				C[cPointer] = B[bPointer];
+				Original[cPointer] = Tail[bPointer];
 				bPointer++;
 				cPointer++;
 			}
+			else if( compareTo(Head[aPointer], Tail[bPointer]) == 0)
+			{
+				Original[cPointer++] = Head[aPointer++];
+				Original[cPointer++] = Tail[bPointer++];
+			}
+
 		}
 
-	while(aPointer < A.length && cPointer < C.length)
+	while(aPointer < Head.length && cPointer < Original.length)
 	{
-		C[cPointer] = A[aPointer];
+		Original[cPointer] = Head[aPointer];
 		aPointer++;
 		cPointer++;
 	}
-
-	while(bPointer < B.length && cPointer < C.length)
+	while(bPointer < Tail.length && cPointer < Original.length)
 	{
-		C[cPointer] = B[bPointer];
+		Original[cPointer] = Tail[bPointer];
 		bPointer++;
 		cPointer++;
 	}
-
 }
 
 }
